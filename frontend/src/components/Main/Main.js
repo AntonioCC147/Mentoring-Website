699332339e8cd03e114740293ea97f6467b7da8b
AntonioCC147 <77618117+AntonioCC147@users.noreply.github.com>
2024-02-22 09:46:57 +0200

    Update Main.js

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -17,7 +17,7 @@ export default function Main() {
                 <Col sm={12} md={6}>
                     <p className="titleMain1">Salutare tuturor! Sunt</p>
                     <p className="titleMain2">Robert, meditator la Informatică și student la Facultatea de Inginerie în Limbi Străine</p>
-                    <p className="titleMain3">Aici ar trebuie să vină o scurtă descriere despre persoana ta, orice consideri relevant.</p>
+                    <p className="titleMain3">Ai bacul sau ești pasionat de informatică? Nu ezita să mă contactezi pentru a începe meditațiile.</p>
                     <Button variant="outline-light" className="buttonMain" href="#despre">Despre Mine</Button>
                 </Col>
                 <Col sm={12} md={6} className="imageContainer">
